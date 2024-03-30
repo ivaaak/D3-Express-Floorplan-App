acb92863236c7dd2b6388e29cddb75fc6fdfe532
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-03-30 17:20:11 +0200

    feat: floorplan service refactor to routes and using mongoDB collection

--- server/src/services/floorplanService.ts
+++ server/src/services/floorplanService.ts
@@ -1,100 +1,120 @@
-// jsonDataService.ts
-
 import express, { Request, Response } from 'express';
-import { JsonData, Point, Polygon } from '../models/floorplan';
 import { collections } from '../database'; // Assuming you have a database module that exports collections
+import { Point, Polygon } from '../models/floorplan';
 
 const floorplanService = express.Router();
 
 // Define your endpoints here
 floorplanService.get('/', async (req, res) => {
-   const employees = await collections.employees?.find().toArray(); // fix undefined??
-   res.json(employees);
+   try {
+       const floorplans = await collections.floorplans?.find().toArray();
+       res.json(floorplans);
+   } catch (error) {
+       res.status(500).send('Error retrieving floorplans');
+   }
+});
+
+// Heatmap endpoints
+floorplanService.get('/heatmap', async (req: Request, res: Response) => {
+   try {
+       const floorplan = await collections.floorplans?.findOne({}, { projection: { heatmap: 1, _id: 0 } });
+       res.json(floorplan?.heatmap);
+   } catch (error) {
+       res.status(500).send('Error retrieving heatmap');
+   }
+});
+
+floorplanService.post('/heatmap', async (req: Request, res: Response) => {
+   try {
+       const newPoint: Point = req.body;
+       const result = await collections.floorplans?.updateOne({}, { $push: { 'heatmap.map': newPoint } });
+       if (result && result?.modifiedCount > 0) {
+           res.status(201).send(newPoint);
+       } else {
+           res.status(404).send('Floorplan not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error adding point to heatmap');
+   }
+});
+
+floorplanService.put('/heatmap/:id', async (req: Request, res: Response) => {
+   try {
+       const updatedPoint: Point = req.body;
+       const result = await collections.floorplans?.updateOne({ 'heatmap.map.x': parseInt(req.params.id) }, { $set: { 'heatmap.map.$': updatedPoint } });
+       if (result && result?.modifiedCount > 0) {
+           res.send(updatedPoint);
+       } else {
+           res.status(404).send('Point not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error updating point in heatmap');
+   }
+});
+
+floorplanService.delete('/heatmap/:id', async (req: Request, res: Response) => {
+   try {
+       const result = await collections.floorplans?.updateOne({}, { $pull: { 'heatmap.map': { x: parseInt(req.params.id) } } });
+       if (result && result?.modifiedCount > 0) {
+           res.status(204).send();
+       } else {
+           res.status(404).send('Point not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error deleting point from heatmap');
+   }
+});
+
+// Overlays endpoints
+floorplanService.get('/overlays', async (req: Request, res: Response) => {
+   try {
+       const floorplan = await collections.floorplans?.findOne({}, { projection: { overlays: 1, _id: 0 } });
+       res.json(floorplan?.overlays);
+   } catch (error) {
+       res.status(500).send('Error retrieving overlays');
+   }
+});
+
+floorplanService.post('/overlays', async (req: Request, res: Response) => {
+   try {
+       const newPolygon: Polygon = req.body;
+       const result = await collections.floorplans?.updateOne({}, { $push: { 'overlays.polygons': newPolygon } });
+       if (result && result?.modifiedCount > 0) {
+           res.status(201).send(newPolygon);
+       } else {
+           res.status(404).send('Floorplan not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error adding polygon to overlays');
+   }
+});
+
+floorplanService.put('/overlays/:id', async (req: Request, res: Response) => {
+   try {
+       const updatedPolygon: Polygon = req.body;
+       const result = await collections.floorplans?.updateOne({ 'overlays.polygons.id': req.params.id }, { $set: { 'overlays.polygons.$': updatedPolygon } });
+       if (result && result?.modifiedCount > 0) {
+           res.send(updatedPolygon);
+       } else {
+           res.status(404).send('Polygon not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error updating polygon in overlays');
+   }
+});
+
+floorplanService.delete('/overlays/:id', async (req: Request, res: Response) => {
+   try {
+       const result = await collections.floorplans?.updateOne({}, { $pull: { 'overlays.polygons': { id: req.params.id } } });
+       if (result && result?.modifiedCount > 0) {
+           res.status(204).send();
+       } else {
+           res.status(404).send('Polygon not found');
+       }
+   } catch (error) {
+       res.status(500).send('Error deleting polygon from overlays');
+   }
 });
 
 // Export the router
 export default floorplanService;
-
-// const jsonData: JsonData = {
-//  heatmap: {
-//     binSize: 1.5,
-//     units: "\u00B0C",
-//     map: [
-//       { x: 2, y: 22, value: 22 },
-//       // Additional data points...
-//     ],
-//  },
-//  overlays: {
-//     polygons: [
-//       { id: "p1", name: "kitchen", points: [
-//         { x: 1.2, y: 11.0 },
-//         // Additional points...
-//       ]},
-//     ],
-//  },
-// };
-
-// app.use(express.json());
-
-// app.get('/heatmap', (req: Request, res: Response) => {
-//  res.json(jsonData.heatmap);
-// });
-
-// app.post('/heatmap', (req: Request, res: Response) => {
-//  const newPoint: Point = req.body;
-//  jsonData.heatmap.map.push(newPoint);
-//  res.status(201).send(newPoint);
-// });
-
-// app.put('/heatmap/:id', (req: Request, res: Response) => {
-//  const updatedPoint: Point = req.body;
-//  const index = jsonData.heatmap.map.findIndex(point => point.x === parseInt(req.params.id));
-//  if (index !== -1) {
-//     jsonData.heatmap.map[index] = updatedPoint;
-//     res.send(updatedPoint);
-//  } else {
-//     res.status(404).send('Point not found');
-//  }
-// });
-
-// app.delete('/heatmap/:id', (req: Request, res: Response) => {
-//  const index = jsonData.heatmap.map.findIndex(point => point.x === parseInt(req.params.id));
-//  if (index !== -1) {
-//     jsonData.heatmap.map.splice(index, 1);
-//     res.status(204).send();
-//  } else {
-//     res.status(404).send('Point not found');
-//  }
-// });
-
-// app.get('/overlays', (req: Request, res: Response) => {
-//  res.json(jsonData.overlays);
-// });
-
-// app.post('/overlays', (req: Request, res: Response) => {
-//  const newPolygon: Polygon = req.body;
-//  jsonData.overlays.polygons.push(newPolygon);
-//  res.status(201).send(newPolygon);
-// });
-
-// app.put('/overlays/:id', (req: Request, res: Response) => {
-//  const updatedPolygon: Polygon = req.body;
-//  const index = jsonData.overlays.polygons.findIndex(polygon => polygon.id === req.params.id);
-//  if (index !== -1) {
-//     jsonData.overlays.polygons[index] = updatedPolygon;
-//     res.send(updatedPolygon);
-//  } else {
-//     res.status(404).send('Polygon not found');
-//  }
-// });
-
-// app.delete('/overlays/:id', (req: Request, res: Response) => {
-//  const index = jsonData.overlays.polygons.findIndex(polygon => polygon.id === req.params.id);
-//  if (index !== -1) {
-//     jsonData.overlays.polygons.splice(index, 1);
-//     res.status(204).send();
-//  } else {
-//     res.status(404).send('Polygon not found');
-//  }
-// });
-
