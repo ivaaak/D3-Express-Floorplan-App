bd1c235284d91a25b6c0741c9b5bafc72f211d1f
Ivaylo Pavlov <43663336+ivaaak@users.noreply.github.com>
2024-03-30 17:20:47 +0200

    feat: startup cleanup, db migration optional

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import routes from "./services/routes";
 import cors from "cors";
 import { connectToDatabase } from "./database";
-import routes from "./services/routes";
+import { migrateFloorplanData } from './database'; // Import the migration function
 
 // Load environment variables from the .env file, where the ATLAS_URI is configured
 dotenv.config();
-
 const { ATLAS_URI } = process.env;
+const app = express();
+const port = 5200;
 
 if (!ATLAS_URI) {
   console.error(
@@ -16,20 +18,19 @@ if (!ATLAS_URI) {
   process.exit(1);
 }
 
-connectToDatabase(ATLAS_URI)
-  .then(() => {
 
-    const app = express();
-    const port = 5200;
-    
-    // Use the routes module
-    app.use(cors());
-    app.use(express.json()); // For parsing application/json
-    app.use('/api', routes); // Prefix all routes with '/api'
-    
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
+async function startServer() {
+  await connectToDatabase(ATLAS_URI!); // Connect to the database
+  // await migrateFloorplanData(); // Migrate floorplan data
 
-  })
-  .catch((error) => console.error(error));
+  // Use the routes module
+  app.use(cors());
+  app.use(express.json()); // For parsing application/json
+  app.use('/api', routes); // Prefix all routes with '/api'
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+startServer().catch((error) => console.error(error));
